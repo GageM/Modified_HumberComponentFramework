0ccfd78e46d4d5ea084df4ad6342fabaca776d9c
GageM <morash.gage@gmail.com>
2023-11-22 19:58:25 -0500

    - Tallied up memory preallocation in Memory Manager  - Added 2 extra memory blocks to allow for optimizing memory

--- ComponentFramework/MemoryManager.h
+++ ComponentFramework/MemoryManager.h
@@ -2,26 +2,6 @@
 #include <iostream>
 
 #include <memory>
-#define KB (1024)
-#define MB (1024 * 1024)
-#define GB (1024 * 1024 * 1024)
-
-
-// THESE VALUES CONTROL THE SIZE OF EACH MEMORY SEGMENT
-// AVAILABLE MEMORY IN EACH SEGMENT WILL BE 1 BYTE LESS Eg, 64 Bytes -> 63 Bytes
-#define Size0 64
-#define Size1 128
-#define Size2 512
-#define Size3 (10 * KB)
-#define Size4 (2 * MB)
-
-// THESE VALUES CONTROL THE AMOUNT OF MEMORY SEGMENTS OF EACH SIZE
-#define Count0 55000
-#define Count1 2000
-#define Count2 500
-#define Count3 100
-#define Count4 30
-
 
 class MemoryMonitor
 {
@@ -53,6 +33,31 @@ protected:
 	bool enablePrintText;
 };
 
+// Memory Manager Values
+#define KB (1024)
+#define MB (1024 * 1024)
+#define GB (1024 * 1024 * 1024)
+
+// THESE VALUES CONTROL THE SIZE OF EACH MEMORY SEGMENT
+// AVAILABLE MEMORY IN EACH SEGMENT WILL BE 1 BYTE LESS Eg, 64 Bytes -> 63 Bytes
+#define Size0 64
+#define Size1 128
+#define Size2 256
+#define Size3 512
+#define Size4 1 * KB
+#define Size5 64 * KB
+#define Size6 2 * MB
+
+// THESE VALUES CONTROL THE AMOUNT OF MEMORY SEGMENTS OF EACH SIZE
+#define Count0 55000	// ~3MB
+#define Count1 8000		// ~1MB
+#define Count2 2000		// 500KB
+#define Count3 300		// ~1.5MB
+#define Count4 100		// 100KB
+#define Count5 100      // ~6MB 
+#define Count6 20       // 40MB
+
+						// ~52MB Total
 class MemoryManager
 {
 public:
@@ -74,12 +79,14 @@ public:
 	char blockSet2[Size2 * Count2];
 	char blockSet3[Size3 * Count3];
 	char blockSet4[Size4 * Count4];
+	char blockSet5[Size5 * Count5];
+	char blockSet6[Size6 * Count6];
 
 private:
-	size_t segmentCount[5]{ Count0, Count1, Count2, Count3, Count4 };
-	size_t segmentSize[5]{ Size0, Size1, Size2, Size3, Size4 };
-	char* memoryBlocks[5]{ blockSet0, blockSet1, blockSet2, blockSet3, blockSet4 };
-	int blockCount = 5;
+	size_t segmentCount[7]{ Count0, Count1, Count2, Count3, Count4, Count5, Count6 };
+	size_t segmentSize[7]{ Size0, Size1, Size2, Size3, Size4, Size5, Size6 };
+	char* memoryBlocks[7]{ blockSet0, blockSet1, blockSet2, blockSet3, blockSet4, blockSet5, blockSet6 };
+	int blockCount = 7;
 
 	MemoryManager();
 };
